39cb87ea3898470ae65154657a40d7943f4dfe89
Abhinav Pottabathula <apottabathula@berkeley.edu>
2020-09-10 19:23:37 -0700

    Adding EEG Reading struct

--- main.py
+++ main.py
@@ -21,12 +21,21 @@ def receiveTest(msg):
 
 @socketio.on('eeg-stream')
 def receiveStream(reading):
-    reading = json.loads(reading)
+    reading = EEGReading(reading)
     print("Received Stream Reading: " + str(reading))
-    if (max(reading['samples']) >= 95):
+    processJawClench(reading)  
+
+def processBlink(reading):
+    if (max(reading.samples) >= 300):
         emit('blink-state', 'eyes_open')
     else:
         emit('blink-state', 'eyes_closed')
+
+def processJawClench(reading):
+    if (max(reading.samples) >= 250):
+        emit('jaw-state', 'clenched')
+    # else:
+    #     emit('jaw-state', 'relaxed')
 
 if __name__ == "__main__":
     socketio.run(app, debug=True)
@@ -35,3 +44,10 @@ if __name__ == "__main__":
 # import upload_model
 # import deploy_model
 # import stream_connect
+
+class EEGReading:
+    def __init__(self, reading):
+        reading = json.loads(reading)
+        self.electrode = reading['electrode']
+        self.timestamp = reading['timestamp']
+        self.samples = reading['samples']
